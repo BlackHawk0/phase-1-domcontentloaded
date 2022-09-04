87650155ce196ee7c448c6f459d877ef4e54a1c4
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-09-04 21:54:33 +0300

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// Your code goes here'JavaScript is so cool. It lets me add text to my page programmatically.'
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const p = document.getElementById("text");
+  p.textContent = "This is really cool!";
+});
